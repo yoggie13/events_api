5258ce6615bc4a19f281fc2179c3f1556fec65f1
yoggie13 <ognjennikolic13@gmail.com>
2022-04-16 01:49:14 +0200

    maybe

--- api.py
+++ api.py
@@ -1,13 +1,13 @@
 from flask import Flask
-from flask_restful import Resource, Api, reqparse
-from flask_cors import CORS
+from flask_restful import reqparse
+# from flask_cors import CORS
 import pandas as pd
 import csv
 import loc_scrapper
 import events_scrapper
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
 # api = Api(app)
 
 
