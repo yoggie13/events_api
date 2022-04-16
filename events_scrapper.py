774460c16fa734f8105aba98ae8819dd04b50bd7
yoggie13 <ognjennikolic13@gmail.com>
2022-04-16 12:01:07 +0200

    changes to driver

--- events_scrapper.py
+++ events_scrapper.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
+import os
 
 
 def returnIndex(month):
@@ -33,7 +34,14 @@ def getPic(_driver, link):
 
 
 def scrape_events(loc_id, link):
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+
+    driver = webdriver.Chrome(service=Service(
+        executable_path=os.environ.get("CHROMEDRIVER_PATH")), options=chrome_options)
     driver.get(link)
 
     soup = BeautifulSoup(driver.page_source, "html.parser")
